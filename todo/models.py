79c608dd857057b4d1e154fc2195d1ca3f7014de
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-01-13 11:14:03 +0500

    Updated models

--- todo/models.py
+++ todo/models.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['-pk']
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo')
