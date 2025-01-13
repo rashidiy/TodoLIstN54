012cd8c96928992ceab3c986ca4ac4f9dd9e86a5
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2025-01-13 11:12:56 +0500

    Added permission and updated todo list view

--- todo/views.py
+++ todo/views.py
@@ -1,21 +1,29 @@
 import json
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, View
-
 from todo.models import Todo
 
 
 class TodoListView(ListView):
-    queryset = Todo.objects.all()
+    model=Todo
+    context_object_name = 'object_list'
     template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            user = self.request.user
+            context['object_list']=Todo.objects.filter(user=user)
+        else:
+            context['object_list']=[]
+        return context
 
 
-class CustomView(View):
 
+class CustomView(LoginRequiredMixin, View):
     def dispatch(self, request, *args, **kwargs):
         if not request.content_type == "application/json":
             return JsonResponse({'status': 'failed', 'error': 'Unsupported content-type'})
@@ -28,9 +36,12 @@ class TodoCreate(CustomView):
 
     def post(self, request: WSGIRequest, *args, **kwargs):
         title = json.loads(request.body).get('title')
+        user = self.request.user
         if not title:
             return JsonResponse({'status': 'failed', 'error': 'Missed key named "title"'}, status=400)
-        todo = Todo.objects.create(title=title)
+        if not user:
+            return JsonResponse({'status': 'failed', 'error': 'Missed key named "user"'}, status=400)
+        todo = Todo.objects.create(title=title, user=user)
         return JsonResponse({'status': 'success', 'id': todo.id})
 
 
